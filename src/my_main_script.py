0064e381074a26e443d3826b6bd963be35bffb17
Maxim Kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-09-01 19:32:12 +0300

    working version

--- src/my_main_script.py
+++ src/my_main_script.py
@@ -7,12 +7,18 @@ my_app = sly.AppService()
 
 LENGTH = int(os.environ['modal.state.length'])
 
-
-@my_app.callback("generate")
 @sly.timeit
-def generate_random_string(api: sly.Api, task_id, context, state):
+@my_app.callback("generate")
+def generate_random_string(api: sly.Api, task_id, context, state, app_logger):
     rand_string = ''.join((random.choice(string.ascii_letters + string.digits)) for _ in range(LENGTH))
+    rand_string = state["prefix"] + rand_string
     api.task.set_field(task_id, "data.randomString", rand_string)
+
+
+@sly.timeit
+@my_app.callback("preprocessing")
+def preprocessing(api: sly.Api, task_id, context, state, app_logger):
+    sly.logger.info("do something here")
 
 
 def main():
@@ -25,13 +31,14 @@ def main():
     }
 
     state = {
+        "prefix": "abc_"
     }
 
     initial_events = [
         {
             "state": None,
             "context": None,
-            "command": "calc",
+            "command": "preprocessing",
         }
     ]
 
