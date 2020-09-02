351bc139ffce6a42bcc2c229ad052af2ddd09975
Maxim Kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-09-02 10:02:04 +0300

    fix timeit

--- src/my_main_script.py
+++ src/my_main_script.py
@@ -7,16 +7,17 @@ my_app = sly.AppService()
 
 LENGTH = int(os.environ['modal.state.length'])
 
-@sly.timeit
+
 @my_app.callback("generate")
+@sly.timeit
 def generate_random_string(api: sly.Api, task_id, context, state, app_logger):
     rand_string = ''.join((random.choice(string.ascii_letters + string.digits)) for _ in range(LENGTH))
     rand_string = state["prefix"] + rand_string
     api.task.set_field(task_id, "data.randomString", rand_string)
 
 
-@sly.timeit
 @my_app.callback("preprocessing")
+@sly.timeit
 def preprocessing(api: sly.Api, task_id, context, state, app_logger):
     sly.logger.info("do something here")
 
