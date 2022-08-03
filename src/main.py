4e4d5aa2cc2dd914b7666914c45e0e9a7b9929ed
Maxim Kolomeychenko <maxim@supervise.ly>
2022-08-03 19:08:56 +0300

    update

--- src/main.py
+++ src/main.py
@@ -2,12 +2,13 @@ import os
 from dotenv import load_dotenv
 from art import tprint
 
+# load ENV variables for debug
 load_dotenv("local.env")
 
 
 def main():
     name = os.environ["context.userLogin"]
-    print("Hello World! This app is run by user:")
+    print("Hello World! This app is run by the user:")
     tprint(name)
 
 
